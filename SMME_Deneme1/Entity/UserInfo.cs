569db35ed8fc5885aa13e1a369de455e66bf0243
Serkan <sekoylmz14@gmail.com>
2020-06-15 17:34:02 +0300

    Facebook Api Integration

---
+++ SMME_Deneme1/Entity/UserInfo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMME_Deneme1.Entity
+{
+    public class UserInfo
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Birthday { get; set; }
+        public Friends Friends { get; set; }
+        public Location Location { get; set; }
+    }
+
+    public class Friends
+    {
+        public Summary Summary { get; set; }
+    }
+
+    public class Summary
+    {
+        public int total_count { get; set; }
+    }
+    public class Location
+    {
+        public string Name { get; set; }
+    }
+
+
+    public class PostData
+    {
+        public string message { get; set; }
+        public string created_time { get; set; }
+        public string full_picture { get; set; }
+    }
+
+
+    public class LikeData
+    {
+        public string Name { get; set; }
+        public string Link { get; set; }
+    }
+}
--- SMME_Deneme1/facebookLogin.cs
+++ SMME_Deneme1/facebookLogin.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Facebook.User;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,8 @@ namespace SMME_Deneme1
         public facebookLogin()
         {
             InitializeComponent();
+            this.txtEmail.Text = "sekoyilmaz14@hotmail.com";
+            this.txtPassword.Text = "yilkar1312";
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -24,7 +27,7 @@ namespace SMME_Deneme1
 
         private void backicon_Click(object sender, EventArgs e)
         {
-            
+
             girisEkranı grs = new girisEkranı();
             this.Hide();
             grs.Show();
@@ -59,6 +62,37 @@ namespace SMME_Deneme1
         }
 
         private void button1_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                if (String.IsNullOrEmpty(this.txtEmail.Text) || String.IsNullOrEmpty(this.txtPassword.Text))
+                {
+                    // Dialog 
+
+                    //returnu silme
+                    return;
+                }
+
+
+                var user = new UserEndpoint(username: this.txtEmail.Text, password: this.txtPassword.Text);
+                facebookManager face = new facebookManager(user);
+                this.Hide();
+                face.Show();
+
+            }
+            catch (Exception ex)
+            {
+                //Dialog çıkar ve şifre veya username hatalı
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void facebookLogin_Load(object sender, EventArgs e)
         {
 
         }
